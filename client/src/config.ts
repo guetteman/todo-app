8f2467b8ecd360ae691512e2980c00daf3fbd849
Luis Güette <guetteluis@gmail.com>
2020-01-13 00:40:09 -0400

    added cors to delete and update URL

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'cxcbka1bq3'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'guetteluis.auth0.com',            // Auth0 domain
+  clientId: 'O8kSVNKEHLoEhYv01FQO4AP0545oFhPL',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
